030882cdb8600ee5e839dbb1e59a29a8a2d52506
jithin101 <153809547+jithin101@users.noreply.github.com>
2026-03-14 00:55:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,24 @@
 import streamlit as st
+import pickle
+import pandas as pd
 
 st.title("Employment Prediction App")
-st.write("Streamlit is working!")
+
+# Load model
+model = pickle.load(open("model.pkl","rb"))
+
+# User inputs
+age = st.number_input("Enter Age")
+experience = st.number_input("Enter Experience")
+
+# Prediction button
+if st.button("Predict"):
+
+    data = pd.DataFrame({
+        "age":[age],
+        "experience":[experience]
+    })
+
+    prediction = model.predict(data)
+
+    st.success(f"Prediction: {prediction[0]}")
